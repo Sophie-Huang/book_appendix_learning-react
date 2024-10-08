535f56f32f62333ea12c3c1d423d515dc3f57ede
Sophie Huang <sophie6506@hotmail.com>
2024-10-08 06:25:31 +0000

    sophie_2024-10-06_06

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,39 @@
 var path = require("path");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: "./src/index.js",
-  output: {
-    path: path.join(__dirname, "dist", "assets"),
-    filename: "bundle.js",
-  },
-  // development 環境最佳選擇為 cheap-module-eval-source-map
-  // devtool: 'eval-source-map', // 如果有列指向的需求，可使用 eval-source-map
-  // devtool: 'cheap-module-eval-source-map', // 此類型的 sourcemap 選項只包含行的指向，並不包含列的指向
+	entry: "./src/index.js",
+	output: {
+		path: path.join(__dirname, "dist", "assets"),
+		filename: "bundle.js",
+	},
+	// development 環境最佳選擇為 cheap-module-eval-source-map
+	// devtool: 'eval-source-map', // 如果有列指向的需求，可使用 eval-source-map
+	// devtool: 'cheap-module-eval-source-map', // 此類型的 sourcemap 選項只包含行的指向，並不包含列的指向
 
-  // production 環境推薦可使用 source-map、hidden-source-map、nosources-source-map
-  devtool: "source-map",
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /(node_modules)/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env", "@babel/preset-react"],
-          },
-        },
-      },
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
-    ],
-  },
+	// production 環境推薦可使用 source-map、hidden-source-map、nosources-source-map
+	devtool: "source-map",
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /(node_modules)/,
+				use: {
+					loader: "babel-loader",
+					options: {
+						presets: ["@babel/preset-env", "@babel/preset-react"],
+					},
+				},
+			},
+			{
+				test: /\.css$/i,
+				use: ["style-loader", "css-loader"],
+			},
+		],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: "./chapter-05/recipe-app/dist/index.html",
+		}),
+	],
 };
